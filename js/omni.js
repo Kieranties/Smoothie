1d449e25a4e77c9e528c6203836630dc7ef4c04e
Kieranties <Kieran@Kieranties.com>
2013-08-13 21:18:38 +0100

    Trimmed down to basic logic

--- js/omni.js
+++ js/omni.js
@@ -6,22 +6,11 @@
 // 'ng' must be explicitly added so $q, $http etc is set
 // see http://docs.angularjs.org/api/angular.injector
 angular.injector(['ng', 'format', 'rtm', 'chrome'])
-    .invoke(function(Format, Rtm, Chrome, $q){
+    .invoke(function(Format, Rtm, Chrome){
 
-        function authCheck(){
-            var d = $q.defer();
-            Rtm.hasAuth().then(
-                function(){ d.resolve(); },
-                function(){
-                    d.reject();
-                    console.log("NO AUTH");
-            });
-            return d.promise;
+        function handleError(data){
+            console.error(data);
         }
-
-        Chrome.omnibox.onInputStarted.addListener(function(){
-            authCheck();
-        });
 
         //suggest on user updates
         Chrome.omnibox.onInputChanged.addListener(function(text, callback){
@@ -31,18 +20,15 @@ angular.injector(['ng', 'format', 'rtm', 'chrome'])
 
         //submit on enter
         Chrome.omnibox.onInputEntered.addListener(function(text){
-            // check we have authentication
-            authCheck()
-                .then(function(){
-                    return Rtm.getData("rtm.timelines.create")
-                })
+
+            Rtm.getData("rtm.timelines.create")
                 .then(function(rsp){
                     return Rtm.getData("rtm.tasks.add", {timeline: rsp.timeline, name: text, parse: 1})
-                })
+                }, handleError)
                 .then(function(rsp){
                     console.log(rsp);
                     // display a notification on success
                     //notify(rsp.list);
-                })
-            });
+                }, handleError)
+        });
    })
